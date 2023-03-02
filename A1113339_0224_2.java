f58770f5460a3d98d0a9878d69c0b53c7a17e737
kennylouis29 <125714859+kennylouis29@users.noreply.github.com>
2023-03-02 23:29:24 +0800

    Add files via upload

---
+++ A1113339_0224_2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class A1113339_0224_2 
+{
+		public static void main(String [] args)
+		{
+				int num;
+
+				System.out.print("enter the numbers in celcius: ");
+				Scanner sc = new Scanner(System.in);
+				num = sc.nextInt();
+				
+				int apply = (((num*9)/5)+32);
+				
+				System.out.println(+apply+ " Farenheit ");
+			}
+}
